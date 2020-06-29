32328d7fd6507992663d9944f670b1efcb5b499c
Carlos Enrique Cimino <ciminoce@yahoo.com.ar>
2020-06-29 18:36:45 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicios05Punto01Version01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ejercicios05Punto01Version01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title="Primer ejemplo de Ciclos Repetitivos";
+            var suma = 0;
+            var contador = 0;
+            while (contador<4)
+            {
+                Console.Write($"Ingrese el número {contador+1}:");
+                var nro =int.Parse(Console.ReadLine());
+                suma = suma + nro;
+                contador = contador + 1;
+
+            }
+            Console.Write($"La suma de los 10 números es {suma}");
+		}
+    }
+}
