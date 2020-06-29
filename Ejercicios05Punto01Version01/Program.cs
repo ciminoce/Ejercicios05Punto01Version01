0e3b7271a8709d427b9c9cde3b2e54c0a53bd3d6
Carlos Enrique Cimino <ciminoce@yahoo.com.ar>
2020-06-29 18:39:35 -0300

    Primer ejermplo de Ciclo

--- Ejercicios05Punto01Version01/Program.cs
+++ Ejercicios05Punto01Version01/Program.cs
@@ -9,12 +9,13 @@ namespace Ejercicios05Punto01Version01
             Console.Title="Primer ejemplo de Ciclos Repetitivos";
             var suma = 0;
             var contador = 0;
-            while (contador<4)
+            while (contador<10)
             {
                 Console.Write($"Ingrese el número {contador+1}:");
                 var nro =int.Parse(Console.ReadLine());
-                suma = suma + nro;
-                contador = contador + 1;
+                suma += nro;//suma=suma+nro;
+                
+                contador++;//contador = contador + 1;
 
             }
             Console.Write($"La suma de los 10 números es {suma}");
